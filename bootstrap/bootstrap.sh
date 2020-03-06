4349727b976be5db5e6e72277a7903b8bc03e261
David Fischer <david@fisch3r.net>
2020-03-06 14:03:38 +0100

    Fix nginx/build.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,7 +5,7 @@ set -e
 ansible-galaxy install -r roles/requirements.yml --roles-path roles --force
 
 ANSIBLE_FILTER_PLUGINS=roles/lib/plugins/filter:/usr/share/ansible/plugins/filter \
-ANSIBLE_LIBRARY=roles/lib/library/:/usr/share/ansible/plugins/modules \
+ANSIBLE_LIBRARY=roles/lib/library:/usr/share/ansible/plugins/modules \
 ANSIBLE_ROLES_PATH=roles \
 ANSIBLE_STDOUT_CALLBACK=debug \
 ansible-playbook bootstrap.yml "$@"
--- nginx/build.sh
+++ nginx/build.sh
@@ -4,8 +4,9 @@ set -e
 
 ansible-galaxy install -r roles/requirements.yml --roles-path roles --force
 
+sudo \
 ANSIBLE_FILTER_PLUGINS=roles/lib/plugins/filter:/usr/share/ansible/plugins/filter \
-ANSIBLE_LIBRARY=roles/lib/library/:/usr/share/ansible/plugins/modules \
+ANSIBLE_LIBRARY=roles/lib/library:/usr/share/ansible/plugins/modules \
 ANSIBLE_ROLES_PATH=roles \
 ANSIBLE_STDOUT_CALLBACK=debug \
-sudo ansible-bender build build.yml "$@"
+ansible-bender build build.yml "$@"
